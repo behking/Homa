6c422685dad2b3bdeae797f5c2047565e8aa885b
Homakia <iranvt@gmail.com>
2025-11-24 18:04:11 +0330

    Add game files

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,7 +2,7 @@ import { http, createConfig } from "wagmi";
 import { injected } from "wagmi/connectors";
 
 // Soneium Minato Testnet
-const soneiumMinato = {
+export const soneiumMinato = {
   id: 1946,
   name: 'Soneium Minato',
   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
@@ -25,8 +25,36 @@ export const config = createConfig({
   },
 });
 
-declare module "wagmi" {
-  interface Register {
-    config: typeof config;
-  }
-}
+// // ---- اضافه کردن تابع کمکی برای سوییچ شبکه ----
+// export const switchToSoneium = async (): Promise<boolean> => {
+//   if (!window.ethereum) {
+//     alert("لطفاً یک کیف پول مثل MetaMask نصب کنید.");
+//     return false;
+//   }
+
+//   try {
+//     await window.ethereum.request({
+//       method: "wallet_switchEthereumChain",
+//       params: [{ chainId: "0x792" }], // 1946 در هگز
+//     });
+//     return true;
+//   } catch (switchError: any) {
+//     if (switchError.code === 4902) {
+//       try {
+//         await window.ethereum.request({
+//           method: "wallet_addEthereumChain",
+//           params: [{
+//             chainId: "0x792",
+//             chainName: soneiumMinato.name,
+//             rpcUrls: soneiumMinato.rpcUrls.default.http,
+//             nativeCurrency: soneiumMinato.nativeCurrency,
+//             blockExplorerUrls: [soneiumMinato.blockExplorers.default.url],
+//           }],
+//         });
+//         return true;
+//       } catch (addError) {
+//         console.error("خطا در اضافه کردن شبکه:", addError);
+//         return false;
+//       }
+//     } else {
+//       console.error("خطا در تغییر شبکه:", switchError);
